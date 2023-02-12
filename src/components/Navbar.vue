e7772fdb40179dab312d26741e86189bc882359d
Ivan <ivan.shaw695@gmail.com>
2023-02-12 19:04:33 +0000

    Fixed logout, before window.tauri test

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -37,7 +37,7 @@ import { RouterLink, RouterView } from "vue-router";
         </li>
         <!-- Make it so this appears if logged in is true -->
         <li class="nav-item" v-if="users.logged_in">
-          <a href="#" @click="this.user.logout()" class="nav-link px-2"
+          <a href="#" @click="user.logout()" class="nav-link px-2"
             >Logout</a
           >
         </li>
